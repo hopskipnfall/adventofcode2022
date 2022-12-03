5a3c5f815d96bebe6304f815a85692cbcbded126
Jonn <hopskipnfall@users.noreply.github.com>
2022-12-03 15:37:20 +0900

    day 1

---
+++ day1.kts
@@ -0,0 +1,18 @@
+import java.io.File
+
+val input = readFile("data/day1.txt")
+
+println("part 1")
+
+println(
+  input.split("\n\n").map { it.split("\n").mapNotNull { it.toIntOrNull() }.sum() }.maxOrNull()
+)
+
+println("part 2")
+
+println(
+  input.split("\n\n").map { it.split("\n").mapNotNull { it.toIntOrNull() }.sum() }.asSequence()
+    .sortedDescending().take(3).sum()
+)
+
+fun readFile(path: String): String = File(path).bufferedReader().use { it.readText() }
